33cc80eecf1ea2ae26a667ee76cf23d68637e1de
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-05-22 23:55:55 -0400

    Added convert a string to an array

---
+++ 8kyu/convert-string-to-array.js
@@ -0,0 +1,11 @@
+// Description:
+// Write a function to split a string and convert it into an array of words.
+// Examples:
+
+// * "Robin Singh" ==> ["Robin", "Singh"]
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+  let arr = string.split(' ');
+  return arr;
+}
